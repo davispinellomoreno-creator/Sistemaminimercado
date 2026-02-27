767d4cee962c3ab92bb5bc7425bae17ef3f02ce2
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-26 22:43:59 -0300

    primeiro commit

---
+++ demo/src/main/java/com/minimercado/demo/MinimercadoApplication.java
@@ -0,0 +1,13 @@
+package com.minimercado.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimercadoApplication.class, args);
+	}
+
+}
