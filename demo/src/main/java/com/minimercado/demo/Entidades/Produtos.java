7ec0aabedb8c32e87b8789b1a4e6a4ab8936457b
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-27 23:51:43 -0300

    Segundo dia de projeto

--- demo/src/main/java/com/minimercado/demo/Entidades/Produtos.java
+++ demo/src/main/java/com/minimercado/demo/Entidades/Produtos.java
@@ -1,8 +1,9 @@
 package com.minimercado.demo.Entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,5 +13,20 @@ import lombok.*;
 @Entity
 @Table(name = "Cadastro_Produtos")
 public class Produtos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Column(name = "nome",unique = true)
+    private String nome;
+
+    @Column(name = "nome",unique = true)
+    private int validade;
+
+
+
+
+
+
 
 }
