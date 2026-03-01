acaad480a5010b597c47614f64fc6882cc932146
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-28 23:22:13 -0300

    Criação de novas classes e pacotes

--- demo/src/main/java/com/minimercado/demo/Repository/MercadoRepository.java
+++ demo/src/main/java/com/minimercado/demo/Repository/MercadoRepository.java
@@ -1,6 +1,7 @@
 package com.minimercado.demo.Repository;
 
+import com.minimercado.demo.Entidades.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MercadoRepository extends JpaRepository {
+public interface MercadoRepository extends JpaRepository <Produtos, Long > {
 }
---
+++ demo/src/main/java/com/minimercado/demo/Status/Status.java
@@ -0,0 +1,4 @@
+package com.minimercado.demo.Status;
+
+public enum Status {
+}
