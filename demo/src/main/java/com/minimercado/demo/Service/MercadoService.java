031af2e5fdaef7f5f9c69304e362355fd6d537d6
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-02-28 23:33:26 -0300

    Commit: iniciando classe service

--- demo/src/main/java/com/minimercado/demo/Service/MercadoService.java
+++ demo/src/main/java/com/minimercado/demo/Service/MercadoService.java
@@ -1,4 +1,12 @@
 package com.minimercado.demo.Service;
 
+import com.minimercado.demo.Repository.MercadoRepository;
+
 public class MercadoService {
+
+    private final MercadoRepository repositorymercado;
+
+    public MercadoService(MercadoRepository repositorymercado) {
+        this.repositorymercado = repositorymercado;
+    }
 }
