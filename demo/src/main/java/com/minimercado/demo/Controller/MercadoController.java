5316fabbadb024a1c7580d3dd4e6690d4d9026e8
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-03-02 12:12:21 -0300

    Finalizando classe service, falatando peuqenos detalhes, inicio do controller

--- demo/src/main/java/com/minimercado/demo/Controller/MercadoController.java
+++ demo/src/main/java/com/minimercado/demo/Controller/MercadoController.java
@@ -1,9 +1,18 @@
 package com.minimercado.demo.Controller;
 
+import com.minimercado.demo.Service.MercadoService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Mercado")
 public class MercadoController {
+
+    private final MercadoService serviceproduto;
+
+
+    public MercadoController(MercadoService serviceproduto) {
+        this.serviceproduto = serviceproduto;
+    }
 }
--- demo/src/main/java/com/minimercado/demo/Service/MercadoService.java
+++ demo/src/main/java/com/minimercado/demo/Service/MercadoService.java
@@ -4,6 +4,7 @@ package com.minimercado.demo.Service;
 import com.minimercado.demo.Entidades.Produtos;
 import com.minimercado.demo.Repository.MercadoRepository;
 
+
 public class MercadoService {
 
     private final MercadoRepository repositorymercado;
@@ -11,17 +12,40 @@ public class MercadoService {
     public MercadoService(MercadoRepository repositorymercado) {
         this.repositorymercado = repositorymercado;
     }
-    public void salvarProduto(Produtos produto){
+
+    public void salvarProduto(Produtos produto) {
 
         repositorymercado.saveAndFlush(produto);
     }
 
-    public void buscarProduto(Long id){
-        repositorymercado.findById(id)
+    public Produtos buscarProduto(Long id) {
+        return repositorymercado.findById(id)
                 .orElseThrow(
                         () -> new RuntimeException("Produto não encontrado")
                 );
 
+    }
+
+    public void deletarProduto(Long id) {
+        repositorymercado.deleteById(id);
+
+    }
+
+    public Produtos atualizarProdutosPorId(Long id, Produtos produto) {
+        Produtos profutoEntity = repositorymercado.findById(id).
+                orElseThrow(
+                        () -> new RuntimeException("Produto não encontrado")
+                );
+        Produtos produtoAtualizado = Produtos.builder()
+                .id(produto.getId() != null
+                        ? produto.getId() : profutoEntity.getId())
+                .nome((produto.getNome() != null ? produto.getNome() : profutoEntity.getNome())
+                )
+
+                .build();
+
+
+        return repositorymercado.save(produto);
     }
 
 }
