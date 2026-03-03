13897d8c87856113f35f7874a39baac599c8a737
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-03-03 13:16:04 -0300

    Começando a implementação na classe controller

--- demo/src/main/java/com/minimercado/demo/Controller/MercadoController.java
+++ demo/src/main/java/com/minimercado/demo/Controller/MercadoController.java
@@ -1,18 +1,27 @@
 package com.minimercado.demo.Controller;
 
+import com.minimercado.demo.Entidades.Produtos;
 import com.minimercado.demo.Service.MercadoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
 
 @RestController
 @RequestMapping("/Mercado")
 public class MercadoController {
 
     private final MercadoService serviceproduto;
+    private final Produtos produto;
 
-
-    public MercadoController(MercadoService serviceproduto) {
+    public MercadoController(MercadoService serviceproduto, Produtos produto) {
         this.serviceproduto = serviceproduto;
+        this.produto = produto;
+    }
+    @GetMapping
+    public ResponseEntity<Produtos> buscarProduto (@RequestParam Long id){
+     return ResponseEntity.ok(serviceproduto.buscarProduto(id));
     }
 }
