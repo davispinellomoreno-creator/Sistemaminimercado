3a5303d1d3782e8c064303b437e6d5a343bef2bf
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-03-06 22:50:21 -0300

    Começando a criação do dto

--- demo/src/main/java/com/minimercado/demo/Dto/MercadoDto.java
+++ demo/src/main/java/com/minimercado/demo/Dto/MercadoDto.java
@@ -1,4 +1,12 @@
 package com.minimercado.demo.Dto;
 
+import java.time.LocalDate;
+
 public class MercadoDto {
+    private String nome;
+        private Double preco;
+        private LocalDate dataValidade;
+
+
+
 }
