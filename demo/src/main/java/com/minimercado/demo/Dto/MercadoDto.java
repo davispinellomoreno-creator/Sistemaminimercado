4d25e68634437f03bcebef3662f2594663aa1daf
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-03-07 23:21:30 -0300

    Finalizando Classe dto

--- demo/src/main/java/com/minimercado/demo/Dto/MercadoDto.java
+++ demo/src/main/java/com/minimercado/demo/Dto/MercadoDto.java
@@ -1,12 +1,18 @@
 package com.minimercado.demo.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class MercadoDto {
     private String nome;
-        private Double preco;
+    private Double preco;
         private LocalDate dataValidade;
 
-
+    public MercadoDto() {
+    }
 
 }
